83919fbfc8aef947d02b6e2eadfd09d93f2912f8
Yixuan Wang <ywang0506@gmail.com>
2018-08-24 11:32:30 -0400

    final change

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,9 +5,7 @@ class Event < ActiveRecord::Base
     event_hash = Event.joins(:event_organizers).group(:name).count
     event_hash.sort_by {|_key, value| value}.reverse.to_h
   end
-  def people_who_led_event
-    Organizer.joins(:event_organizers).where(event_organizers: {event_id:self.id,is_organizer:true})
-  end
+
   def self.find_event_by_location(entered_description)
     event_array = Event.all.where(location_borough: entered_description)
   end
